929cccdec3bad63c74ad3a1d5051d346408f02ce
Nattapong Unaregul <tylerlantern@gmail.com>
2020-03-27 13:24:46 +0700

    add server project

--- WebSocketTesting/ViewController.swift
+++ WebSocketTesting/ViewController.swift
@@ -13,35 +13,55 @@ class ViewController: UIViewController {
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var textField: UITextField!
   @IBOutlet weak var sendButton: UIButton!
+  var manager : SocketManager!
   
   @IBAction func action_sendMessage(_ sender: Any) {
-    socket.emit("server", with: [textField.text ?? ""])
+//    socket.emit("server", with: [textField.text ?? ""])
   }
-  
-  let manager = SocketManager(socketURL: URL(string: "ws://echo.websocket.org")!, config: [.log(true), .compress])
-  var socket : SocketIO.SocketIOClient  { return manager.defaultSocket }
+//  ws://echo.websocket.org
+//  wss://echo.websocket.org
+//  let manager = SocketManager(
+//    socketURL: URL(string: "ws://echo.websocket.org")!,
+//    config: [
+//      .log(true),
+//      .forceWebsockets(true),
+//    ]
+//
+//  )
+//  var socket : SocketIO.SocketIOClient  { return manager.defaultSocket }
   override func viewDidLoad() {
     super.viewDidLoad()
-  
-    socket.on(clientEvent: .connect) {data, ack in
-        print("On Connect")
-    }
-
-    socket.on(clientEvent: .error) {data, ack in
-        print("On Error:",data)
-    }
-    
-    socket.on("user") { (data, ack) in
-      print("user:",data)
-      ack.with("Got it")
-    }
-    socket.connect()
+    integrateSocket()
+//    manager.defaultSocket.connect()
     // Do any additional setup after loading the view.
   }
 
-  
-  
-  
+  func integrateSocket(){
+    
+    manager = SocketManager(
+      socketURL: URL(string: "wss://echo.websocket.org")!,
+      config: [
+        .log(true),
+        .forceWebsockets(true)
+      ]
+    
+    )
+    
+     manager.defaultSocket.on(clientEvent: .connect) {data, ack in
+        print("On Connect")
+    }
 
+     manager.defaultSocket.on(clientEvent: .error) {data, ack in
+        print("On Error:",data)
+    }
+    
+     manager.defaultSocket.onAny { (event) in
+      print("onAny event:",event.event," items:",event.items)
+    }
+    
+    
+    manager.defaultSocket.connect()
+  }
+  
 }
 
